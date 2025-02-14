6505a0d2321d51a3c104e8f06c50232ca95a9456
Fraser Sharp <fraser.sharp@pm.me>
2025-02-14 01:37:55 +0000

    threaded mandelbrot

--- 02-threads/run.go
+++ 02-threads/run.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"image"
 	"image/color"
+	"math"
 	"time"
 
 	"github.com/go-p5/p5"
@@ -16,13 +17,19 @@ type imageSegment struct {
 }
 
 var (
-	screenWidth         = 1000
+	screenWidth         = 1200
 	screenHeight        = 800
-	threads             = 12
+	threads             = 24
 	fpsUpdateInterval   = 1000 * time.Millisecond
 	nextFpsUpdate       = time.Now().Add(fpsUpdateInterval)
 	frameCount          = 0
 	displayedFrameCount = 0
+
+	mandelbrotScaleX     = float64(1.7)
+	mandelbrotScaleY     = float64(1.5)
+	mandelbrotPositionX  = float64(-2.0)
+	mandelbrotPositionY  = float64(-0.5)
+	mandelbrotIterations = 200
 )
 
 func main() {
@@ -31,7 +38,6 @@ func main() {
 
 func setup() {
 	p5.Canvas(screenWidth, screenHeight)
-	p5.Background(color.Gray{Y: 80})
 }
 
 func draw() {
@@ -40,45 +46,7 @@ func draw() {
 	for thread := range threads {
 		imageChannels[thread] = make(chan imageSegment)
 
-		go func(thread int) {
-			segmentHeight := screenHeight / threads
-			segmentWidth := screenWidth
-
-			segmentTop := segmentHeight * thread
-			segmentLeft := 0
-
-			image := image.NewNRGBA(
-				image.Rectangle{
-					image.Point{
-						0,
-						0,
-					},
-					image.Point{
-						segmentWidth,
-						segmentHeight,
-					},
-				},
-			)
-
-			for x := range image.Rect.Size().X {
-				for y := range image.Rect.Size().Y {
-					image.SetNRGBA(x, y, color.NRGBA{
-						uint8(x + segmentTop + y),
-						uint8(x + segmentTop - y),
-						uint8(x + segmentTop*y),
-						255,
-					})
-				}
-			}
-
-			imageChannels[thread] <- imageSegment{
-				image: image,
-				x:     segmentLeft,
-				y:     segmentTop,
-			}
-
-			close(imageChannels[thread])
-		}(thread)
+		go processSegment(thread, &imageChannels[thread])
 	}
 
 	for thread := range threads {
@@ -87,6 +55,12 @@ func draw() {
 	}
 
 	p5.TextSize(50)
+	p5.Fill(color.NRGBA{
+		200,
+		200,
+		200,
+		255,
+	})
 	p5.Text(fmt.Sprintf("%v fps", displayedFrameCount), 50, 50)
 	frameCount++
 
@@ -95,4 +69,69 @@ func draw() {
 		frameCount = 0
 		nextFpsUpdate = time.Now().Add(fpsUpdateInterval)
 	}
+}
+
+func mandelbrot(x float64, y float64) int {
+	cursorx := float64(0.0)
+	cursory := float64(0.0)
+	iteration := 0
+
+	for {
+		if math.Pow(cursorx, 2)+math.Pow(cursory, 2) >= 4.0 || iteration >= mandelbrotIterations {
+			return iteration
+		}
+
+		xtemp := math.Pow(cursorx, 2) - math.Pow(cursory, 2) + x
+		cursory = 2*cursorx*cursory + y
+		cursorx = xtemp
+		iteration++
+	}
+}
+
+func processSegment(thread int, imageChannel *chan imageSegment) {
+	segmentHeight := screenHeight / threads
+	segmentWidth := screenWidth
+
+	segmentTop := segmentHeight * thread
+	segmentLeft := 0
+
+	image := image.NewNRGBA(
+		image.Rectangle{
+			image.Point{
+				0,
+				0,
+			},
+			image.Point{
+				segmentWidth,
+				segmentHeight,
+			},
+		},
+	)
+
+	for x := range image.Rect.Size().X {
+		for y := range image.Rect.Size().Y {
+			segx := x
+			segy := (segmentHeight * thread) + y
+
+			mbx := ((float64(segx) / float64(segmentWidth)) * mandelbrotScaleX) + mandelbrotPositionX
+			mby := ((float64(segy) / float64(screenHeight)) * mandelbrotScaleY) + mandelbrotPositionY
+
+			mbResult := mandelbrot(mbx, mby)
+
+			image.SetNRGBA(x, y, color.NRGBA{
+				uint8(mbResult * 50),
+				uint8(mbResult * 10),
+				uint8(mbResult * 70),
+				255,
+			})
+		}
+	}
+
+	*imageChannel <- imageSegment{
+		image: image,
+		x:     segmentLeft,
+		y:     segmentTop,
+	}
+
+	close(*imageChannel)
 }
