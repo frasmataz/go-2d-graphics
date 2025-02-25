1ea234afe25d32ab4516c9131a34a7af692ff982
Fraser Sharp <fraser.sharp@pm.me>
2025-02-25 02:07:30 +0000

    add keyboard controls

--- 02-threads/run.go
+++ 02-threads/run.go
@@ -7,13 +7,26 @@ import (
 	"math"
 	"time"
 
-	"github.com/go-p5/p5"
+	"gioui.org/io/key"
+	"github.com/frasmataz/p5"
 )
 
 type imageSegment struct {
 	image image.Image
 	x     int
 	y     int
+}
+
+type mandelbrotState struct {
+	iterations uint
+	zoom       float64
+	pos        struct {
+		x float64
+		y float64
+	}
+
+	panSpeed  float64
+	zoomSpeed float64
 }
 
 var (
@@ -25,11 +38,17 @@ var (
 	frameCount          = 0
 	displayedFrameCount = 0
 
-	mandelbrotScaleX     = float64(1.7)
-	mandelbrotScaleY     = float64(1.5)
-	mandelbrotPositionX  = float64(-2.0)
-	mandelbrotPositionY  = float64(-0.5)
-	mandelbrotIterations = 200
+	panSpeed  = float64(0.1)
+	zoomSpeed = float64(0.05)
+
+	mandelbrot = mandelbrotState{
+		iterations: 20,
+		zoom:       1.5,
+		pos: struct {
+			x float64
+			y float64
+		}{-2.0, -0.5},
+	}
 )
 
 func main() {
@@ -41,6 +60,8 @@ func setup() {
 }
 
 func draw() {
+	processInput()
+
 	imageChannels := make([]chan imageSegment, threads)
 
 	for thread := range threads {
@@ -71,13 +92,34 @@ func draw() {
 	}
 }
 
-func mandelbrot(x float64, y float64) int {
+func processInput() {
+	if p5.KeyIsDown(key.NameLeftArrow) {
+		mandelbrot.pos.x -= (panSpeed * mandelbrot.zoom)
+	} else if p5.KeyIsDown(key.NameRightArrow) {
+		mandelbrot.pos.x += (panSpeed * mandelbrot.zoom)
+	}
+
+	if p5.KeyIsDown(key.NameUpArrow) {
+		mandelbrot.pos.y -= (panSpeed * mandelbrot.zoom)
+	} else if p5.KeyIsDown(key.NameDownArrow) {
+		mandelbrot.pos.y += (panSpeed * mandelbrot.zoom)
+	}
+
+	if p5.KeyIsDown(key.NamePageUp) {
+		mandelbrot.zoom += (zoomSpeed * mandelbrot.zoom)
+	} else if p5.KeyIsDown(key.NamePageDown) {
+		mandelbrot.zoom -= (zoomSpeed * mandelbrot.zoom)
+	}
+
+}
+
+func renderMandelbrot(x float64, y float64) uint {
 	cursorx := float64(0.0)
 	cursory := float64(0.0)
-	iteration := 0
+	iteration := uint(0)
 
 	for {
-		if math.Pow(cursorx, 2)+math.Pow(cursory, 2) >= 4.0 || iteration >= mandelbrotIterations {
+		if math.Pow(cursorx, 2)+math.Pow(cursory, 2) >= 4.0 || iteration >= mandelbrot.iterations {
 			return iteration
 		}
 
@@ -113,10 +155,10 @@ func processSegment(thread int, imageChannel *chan imageSegment) {
 			segx := x
 			segy := (segmentHeight * thread) + y
 
-			mbx := ((float64(segx) / float64(segmentWidth)) * mandelbrotScaleX) + mandelbrotPositionX
-			mby := ((float64(segy) / float64(screenHeight)) * mandelbrotScaleY) + mandelbrotPositionY
+			mbx := ((float64(segx) / float64(segmentWidth) * 1.5) * mandelbrot.zoom) + mandelbrot.pos.x
+			mby := ((float64(segy) / float64(screenHeight)) * mandelbrot.zoom) + mandelbrot.pos.y
 
-			mbResult := mandelbrot(mbx, mby)
+			mbResult := renderMandelbrot(mbx, mby)
 
 			image.SetNRGBA(x, y, color.NRGBA{
 				uint8(mbResult * 50),
